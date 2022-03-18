5a8390cac4bf451a792b6c4813be94dd5cb27860
John Pohl <johnpohl34@gmail.com>
2022-03-18 08:28:46 -0400

    images added

--- controllers/api/basketRoute.js
+++ controllers/api/basketRoute.js
@@ -5,47 +5,48 @@ const withAuth = require('../../utils/auth');
 // retrieve items from certain databse by id
 // user.findOne or user.findAll
 router.post('/:id', withAuth, (req, res) => {
-    try {
-        Order.findAll({
-        })
-        res.status(200).json(newOrder);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+  try {
+    Order.findAll({
+      where: [ order ]
+    });
+    res.status(200).json(newOrder);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 
-    // Product.findAll({
-    //     where: order
+// Product.findAll({
+//     where: order
 });
 
 router.post('/:id', withAuth, (req, res) => {
-    try {
-        User.findOne({
-            include: order
-        })
-        res.status(200).json(newOrder);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+  try {
+    User.findOne({
+      include: order
+    });
+    res.status(200).json(newOrder);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
-    // delete products from basket
-    try {
-        const productData = await Order.destroy({
-            where: {
-                id: req.params.id
-            },
-        });
+  // delete products from basket
+  try {
+    const productData = await Order.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
 
-        if (!productData) {
-            res.status(404).json({ message: 'No product found with that id.' });
-            return;
-        }
-
-        res.status(200).json(productData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id.' });
+      return;
     }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
