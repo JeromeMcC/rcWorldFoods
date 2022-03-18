2e9ef0adc34b5c44ab5671d98a7bb48c3f99bb6b
John Pohl <johnpohl34@gmail.com>
2022-03-17 22:52:54 -0400

    post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 //const basketRoute = require('./basketRoute');
-//const checkoutRoute = require('./checkoutRoute');
+const checkoutRoute = require('./checkoutRoute');
 
 router.use('/users', userRoutes);
+router.use('./checkout', checkoutRoute);
 
 module.exports = router;
